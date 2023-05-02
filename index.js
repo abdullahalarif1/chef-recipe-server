b3ae5008d1530add2f9b71787a70a9ebaabfac62
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-05-02 14:28:34 +0600

    json data create and send to server successfully

--- index.js
+++ index.js
@@ -5,8 +5,23 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
+const chef = require('./Data/chef.json')
+const team = require('./Data/recipes.json')
+const trending = require('./Data/trebding.json')
+
 app.get('/', (req, res) => {
     res.send('server is running')
+})
+
+app.get('/chef', (req, res)=> {
+    res.send(chef)
+})
+app.get('/trending', (req, res)=> {
+    res.send(trending)
+})
+
+app.get('/team', (req, res)=> {
+    res.send(team)
 })
 
 app.listen(port, () => {
